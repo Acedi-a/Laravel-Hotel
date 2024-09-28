105955bc92884772dd3690bb61ca5964e2f0e879
Acedi-a <stardarck15@gmail.com>
2024-09-27 21:45:39 -0400

    color ajustado

--- views/layout.php
+++ views/layout.php
@@ -33,7 +33,7 @@
             USER;
 
         } else {
-            echo "<a href='login' class='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300'><button>Entrar</button></a>";
+            echo "<a href='login' class='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-500 transition duration-300'><button>Entrar</button></a>";
         }
         ?>
     </nav>
