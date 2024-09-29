686e7845eb9f238574e9c60b1c7e831210d6aee2
Acedi-a <stardarck15@gmail.com>
2024-09-29 17:53:09 -0400

    me olvide de agergar este modelo

---
+++ models/tipo_habitacion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Models;
+
+class tipo_habitacion extends ActivarModelo
+{
+    protected static $tabla = 'tipos_habitacion';
+    protected static $columnDB = ['id_tipo','nombre','descripcion'];
+
+    public $nombre;
+    public $descripcion;
+
+    public function __construct($args = []){
+        $this->nombre = $args['nombre'];
+        $this->descripcion = $args['descripcion'];
+
+    }
+
+}
